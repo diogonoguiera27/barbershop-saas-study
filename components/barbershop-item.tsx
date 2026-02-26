b05efbc6eca242b8f58a21565d090451620df0f5
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2026-02-26 12:59:32 -0300

    feat: add quick search

--- components/barbershop-item.tsx
+++ components/barbershop-item.tsx
@@ -12,7 +12,7 @@ interface BarbershopItemProps {
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
     <Card className="min-w-[167px] rounded-2xl">
-      <CardContent className="p-0 px-2 pt-1">
+      <CardContent className="p-0 px-1 pt-1">
         {/* IMAGEM */}
 
         <div className="relative h-[159px] w-full">
@@ -36,7 +36,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
 
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
-          <p className="truncatetext-sm text-gray-400">{barbershop.address}</p>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
           <Button variant="secondary" className="mt-3 w-full">
             Reservar
           </Button>
