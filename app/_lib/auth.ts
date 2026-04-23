23b26cfc1fbaafcaacd152b9d13c14cc6412c981
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2026-04-23 17:03:40 -0300

    chore: add NEXT_AUTH_SECRET env variable

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -11,7 +11,7 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
     async session({ session, token }) {
       if (session.user && token.sub) {
