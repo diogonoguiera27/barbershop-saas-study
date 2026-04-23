ed8cd34a823a138239d01920b4cc23bd3d70bcad
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2026-04-22 23:56:45 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import Search from "@/components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -48,8 +50,19 @@ const Home = async () => {
       <Header />
       <div className="p-5">
         {/*  Texto */}
-        <h2 className="text-xl font-bold">Ola, felipe!</h2>
+        <h2 className="text-xl font-bold">
+          Ola, {session?.user ? session.user.name : "bem vindo"}
+        </h2>
         <p>Segunda Feira, 05 de agosto</p>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/*Busca */}
         <div className="mt-6">
